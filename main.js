e1bc6e30f942ed844573bdcc6a402f1e6158eead
Oleksandr Lysenko <121089151+beastcom74@users.noreply.github.com>
2024-03-25 23:30:54 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         TikTokBC
+// @namespace    http://tampermonkey.net/
+// @version      1.0
+// @description  An extension for the Tiktok site that makes the time spent on the site even better.
+// @author       beastcom
+// @match        https://www.tiktok.com/
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    "use strict";
+
+    window.opener.focus();
+})();
