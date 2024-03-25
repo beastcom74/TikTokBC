c1378fc8df0e02547782d479d795f6a6237d533e
Oleksandr Lysenko <121089151+beastcom74@users.noreply.github.com>
2024-03-25 23:51:51 +0200

    Update main.user.js

--- main.user.js
+++ main.user.js
@@ -5,6 +5,8 @@
 // @description  An extension for the Tiktok site that makes the time spent on the site even better.
 // @author       beastcom
 // @match        https://www.tiktok.com/
+// @updateURL  https://github.com/beastcom74/TikTokBC/raw/main/main.user.js
+// @downloadURL  https://github.com/beastcom74/TikTokBC/raw/main/main.user.js
 // ==/UserScript==
 
 (function () {
